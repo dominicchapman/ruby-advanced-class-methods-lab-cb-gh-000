10aa454821da1fd9e02248dd52fa2182faaded11
Dominic Chapman <dominicchapman@me.com>
2017-10-08 23:05:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    Song.all.detect { |song| song.name == song_name }
+    # Song.all.detect { |song| song.name == song_name }
   end
 
 end
